5fcca609850ffcc332df64d01cd383b3d2289f7e
OsamaKJ <osama-20-56@hotmail.com>
2019-05-11 11:12:17 +0300

    fifng

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'hotel'],function (){
+
+    Route::get('/',['as'=>'index','uses'=>'HotelController@index']);
+
+});
